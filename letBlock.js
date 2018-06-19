4033f77de28442c9da0ea6bb75860927ac8d858e
fendijatmiko <jatmikofendi@gmail.com>
2018-06-19 20:23:38 +0700

    add scope example

---
+++ letBlock.js
@@ -0,0 +1,17 @@
+function allyIterate(){
+    for ( let tuce = 0; tuce <5; tuce ++ ) {
+            //tuce is  only visible in here (and in the for() parentheses )
+            //and there is a separate tuce variable for each iteration of the loop
+        }
+        //tuce is **not** visible out here
+   
+    }
+
+function byE40(){
+    // nish *is* visible out here
+    for (var nish = 0; nish <5; nish++){
+        //nish is visible to the whole function 
+        }    
+        //nish *is* visible out here too
+
+    }
